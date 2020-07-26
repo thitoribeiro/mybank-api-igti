6c89fd09cad837e74669d318fd77314cfe35d5b5
thitoribeiro <thito.ribeiro@hotmail.com>
2020-07-26 19:38:53 -0300

    Post method with json file

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const app = express();
+var fs = require("fs");
+
+app.use(express.json());
 
 
 app.post("/account", (req, res) => {
-  console.log('post account');
+  let params = req.body;
+
+  fs.readFile("accounts.json", "utf8", (err, data) => {
+    console.log(err);
+
+    try {
+      let json = JSON.parse(data);
+      console.log(json);
+
+      res.send("post account");
+    } catch (err) {
+      res.send("erro");
+    }
+
+
+  });
+
+  // fs.appendFile("accounts.json", JSON.stringify(params), err => {
+  //   console.log(err);
+  // });
 });
 
-
-
 app.listen(3000, () => {
+  try {
+    fs.readFile("accounts.json", "utf8", (err, data) => {
+      console.log(err);
+      console.log(data);  
+    });
+  } catch (err) {
+    console.log(err);
+  }
   console.log("API started");
 });
